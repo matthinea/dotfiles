3278a3229cb0ca3d1cae110b4e1d1ff9df9b4ff5
Matthew Hinea <matthew.hinea@gmail.com>
2025-02-27 09:54:33 -0800

    add starrocksprod alias

--- sources/data_axle.zsh
+++ sources/data_axle.zsh
@@ -3,3 +3,6 @@ alias peter='axl'
 
 export CHEF_USER=matth
 
+alias starrocksprod='kubectl -n starrocks-namespace exec -it starrocks-cluster-fe-0 -- \
+  mysql -P 9030 -h 127.0.0.1 -u root -p --prompt="StarRocks [PROD] > "'
+
