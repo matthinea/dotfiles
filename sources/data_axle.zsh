03369bf725a1e42b4cd65316556a24717e8b60b3
Matthew Hinea <matthew.hinea@gmail.com>
2025-03-04 10:40:55 -0800

    StarRocks command - -S for silent start

--- sources/data_axle.zsh
+++ sources/data_axle.zsh
@@ -4,5 +4,5 @@ alias peter='axl'
 export CHEF_USER=matth
 
 alias starrocksprod='kubectl -n starrocks-namespace exec -it starrocks-cluster-fe-0 -- \
-  mysql -P 9030 -h 127.0.0.1 -D data_axle_star_rocks_production -u root --prompt="StarRocks [PROD] > "'
+  mysql -s -P 9030 -h 127.0.0.1 -A -D data_axle_star_rocks_production -u root --prompt="StarRocks [PROD] > "'
 
